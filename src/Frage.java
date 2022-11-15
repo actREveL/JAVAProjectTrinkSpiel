bfc41d7370851089fe8bf403fa044078afda1043
silyr <silyr@bushido.fritz.box>
2022-11-15 19:28:17 +0100

    all classes

--- src/Frage.java
+++ src/Frage.java
@@ -1,12 +1,8 @@
-import javafx.scene.control.CheckBox;
-
 public class Frage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Checkbox
-		CheckBox checkbox = new CheckBox ("si");
 	}
 
 }
